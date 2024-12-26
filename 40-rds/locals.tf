a1c56a97d03077080b46edcf229f135953d1075a
Sankar491 <sivasankarrcks@gmail.com>
2024-12-26 10:45:05 +0530

    terraform

--- 40-rds/locals.tf
+++ 40-rds/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  resource_name = "${var.project}-${var.environment}-bastion"
+  resource_name = "${var.project}-${var.environment}-rds"
   database_subnet_group_name = data.aws_ssm_parameter.database_subnet_group_name.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
 }
